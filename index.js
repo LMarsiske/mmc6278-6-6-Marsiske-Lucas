fbb779577427794a5961037a3548777385b903bd
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2022-06-17 14:13:43 -0400

    Clicking outside the hamburger menu closes it

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const btn = document.getElementsByClassName("hamburger-btn")[0];
-const menu = document.getElementsByClassName("hamburger-menu")[0];
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
+const container = document.getElementsByClassName("menu")[0];
 
-console.log(menu);
+console.log(container);
 
-btn.onclick = function (e) {
-  console.log(btn);
-  if (btn.getAttribute("aria-expanded") === "true") {
+container.onclick = function (e) {
+  e.stopPropagation();
+  if (
+    e.target.tagName === "BUTTON" &&
+    btn.getAttribute("aria-expanded") === "true"
+  ) {
     console.log("expanded true");
     btn.setAttribute("aria-expanded", false);
-    menu.classList.remove("show-menu");
-  } else {
+    hamburgerMenu.classList.remove("show-menu");
+    return;
+  }
+
+  if (
+    e.target.tagName === "BUTTON" &&
+    btn.getAttribute("aria-expanded") === "false"
+  ) {
     console.log("expanded false");
     btn.setAttribute("aria-expanded", true);
-    menu.classList.add("show-menu");
+    hamburgerMenu.classList.add("show-menu");
+    return;
+  }
+};
+
+document.body.onclick = function (e) {
+  if (btn.getAttribute("aria-expanded") === "true") {
+    btn.setAttribute("aria-expanded", false);
+    hamburgerMenu.classList.remove("show-menu");
   }
 };
